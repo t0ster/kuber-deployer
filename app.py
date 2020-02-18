7ff03638fa04b869ebd195f5b6d8fc72866c7de1
Roman Dolgyi <tosters@gmail.com>
2020-02-17 21:41:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,8 @@ async def write_values_yaml(values):
 def get_helm_cmd(command, values_yaml):
     cmd = (
         f'kubectl create ns {command["namespace"]} || true && '
-        f'helm upgrade -i --wait --namespace {command["namespace"]} '
+        f'helm upgrade -i --wait --cleanup-on-fail --force '
+        '--namespace {command["namespace"]} '
         f'{command["release"]} {command["chart"]}'
     )
     cmd += values_yaml and f' -f {values_yaml}' or ''
